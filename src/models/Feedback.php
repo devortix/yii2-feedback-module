fd69c4d6e6a852cdd1f96bef4101ccf8c9966d84
Devils <devortix@gmail.com>
2019-02-21 11:03:13 +0200

    mail send

--- src/models/Feedback.php
+++ src/models/Feedback.php
@@ -6,19 +6,19 @@ use Yii;
 use yii\behaviors\TimestampBehavior;
 
 /**
- * This is the model class for table "{{%feedback}}".
- *
- * @property int $id
- * @property string $user_name
- * @property string $user_email
- * @property string $phone
- * @property string $push_emails
- * @property string $content
- * @property string $info
- * @property int $created_at
- * @property int $updated_at
- * @property int $status
- */
+* This is the model class for table "{{%feedback}}".
+*
+* @property int $id
+* @property string $user_name
+* @property string $user_email
+* @property string $phone
+* @property string $push_emails
+* @property string $content
+* @property string $info
+* @property int $created_at
+* @property int $updated_at
+* @property int $status
+*/
 class Feedback extends \yii\db\ActiveRecord
 {
     const STATUS_NEW = 1;
@@ -31,18 +31,18 @@ class Feedback extends \yii\db\ActiveRecord
         self::STATUS_OK => 'label-success label',
         self::STATUS_CENCEL => 'label-danger label',
     ];
-
+    
     public $file = null;
     public $fileFolder = '@webroot/uploads/feedback/';
     public $file_url = '@web/uploads/feedback/';
     /**
-     * {@inheritdoc}
-     */
+    * {@inheritdoc}
+    */
     public static function tableName()
     {
         return '{{%feedback}}';
     }
-
+    
     public function behaviors()
     {
         return [
@@ -54,10 +54,10 @@ class Feedback extends \yii\db\ActiveRecord
             ],
         ];
     }
-
+    
     /**
-     * {@inheritdoc}
-     */
+    * {@inheritdoc}
+    */
     public function rules()
     {
         return [
@@ -68,32 +68,32 @@ class Feedback extends \yii\db\ActiveRecord
             [['file'], 'file', 'extensions' => 'doc, docx, txt, pdf'],
         ];
     }
-
+    
     public function beforeSave($insert)
     {
         if ($insert) {
             $this->status = 1;
         }
         if (parent::beforeSave($insert)) {
-
+            
             return true;
         } else {
             return false;
         }
     }
-
+    
     public function beforeDelete()
     {
         if (is_file($this->filePath)) {
             unlink($this->filePath);
         }
-
+        
         return parent::beforeDelete();
     }
-
+    
     /**
-     * {@inheritdoc}
-     */
+    * {@inheritdoc}
+    */
     public function attributeLabels()
     {
         return [
@@ -111,26 +111,26 @@ class Feedback extends \yii\db\ActiveRecord
             'file' => Yii::t('admin-feedback', 'File'),
         ];
     }
-
+    
     public function getFilePath()
     {
         
         return Yii::getAlias($this->fileFolder) . $this->file_name;
     }
-
+    
     public function getFileUrl()
     {
         $url = Yii::getAlias($this->file_url);
         return $url . $this->file_name;
     }
-
+    
     public function generateFileName()
     {
         return $this->file_name = \Yii::$app->formatter->asDate($this->created_at, 'php:Y-m-d H:i') . '_'
         . $this->file->baseName
         . '.' . $this->file->extension;
     }
-
+    
     public function upload()
     {
         $this->generateFileName();
@@ -141,7 +141,7 @@ class Feedback extends \yii\db\ActiveRecord
         }
         return false;
     }
-
+    
     public static function getStatuses()
     {
         return [
@@ -149,26 +149,58 @@ class Feedback extends \yii\db\ActiveRecord
             self::STATUS_WAIT => Yii::t('admin-feedback', 'status wait'),
             self::STATUS_OK => Yii::t('admin-feedback', 'status ok'),
             self::STATUS_CENCEL => Yii::t('admin-feedback', 'status cencel'),
-
+            
         ];
     }
-
+    
     public function getStatusClass()
     {
-        if(isset($this->statusClasses[$this->status]))
+        if (isset($this->statusClasses[$this->status])) {
             return $this->statusClasses[$this->status];
+        }
+        
         return '';
     }
-
+    
     public function getStatusLabel()
     {
-        $statuses =  self::getStatuses();
-        if(isset($statuses[$this->status]))
+        $statuses = self::getStatuses();
+        if (isset($statuses[$this->status])) {
             return $statuses[$this->status];
+        }
+        
         return 'null';
     }
-
-    public function getUid(){
+    
+    public function getUid()
+    {
         return '#' . $this->id . ' ' . $this->name;
+    }
+    
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+        if (isset(Yii::$app->params['feedbackFrom']) && isset(Yii::$app->params['feedbackTo']) && $insert) {$message = Yii::$app->mailer->compose();
+            
+            $message->setFrom(Yii::$app->params['feedbackFrom']);
+            
+            $message->setTo(Yii::$app->params['feedbackTo'])
+            ->setSubject($this->name)
+            ->setTextBody($this->message)
+            ->send();
+        }
+    }
+    
+    public function getMessage()
+    {
+        $msg = '';
+        $msg .= (!empty($this->name) ? 'Форма: ' . $this->name . " \n": '');
+        $msg .= 'ID: ' . $this->id . " \n";
+        $msg .= (!empty($this->user_name) ? 'Имя: ' . $this->user_name . " \n" : '');
+        $msg .= (!empty($this->user_email) ? 'Емейл: ' . $this->user_email . " \n" : '');
+        $msg .= (!empty($this->phone) ? 'Телефон: ' . $this->phone . " \n" : '');
+        $msg .= (!empty($this->content) ? 'Комментарий: ' . $this->content . " \n" : '');
+        $msg .= (!empty($this->info) ? 'Остальные поля: \n ' . $this->info . " \n" : " \n");
+        return $msg;
     }
 }
